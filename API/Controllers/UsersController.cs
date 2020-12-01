f78f0f607b215fe1660e0fc4bbe1bcf0164b1a40
Pingvinberg <Pingvinberg@gmail.com>
2020-12-02 00:45:37 +0100

    client booststrap and awesome font

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -11,11 +11,11 @@ namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class UserController : ControllerBase
+    public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public UserController(DataContext context)
+        public UsersController(DataContext context)
         {
             _context = context;
         }
