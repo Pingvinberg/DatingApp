4eb61cd399fb7bb986587aa9de68b66908fa8b89
Pingvinberg <Pingvinberg@gmail.com>
2021-02-17 17:51:29 +0100

    Added Roles / Admin-management

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -30,7 +30,7 @@ namespace API.Controllers
             _photoService = photoService;
             _userRepository = userRepository;
         }
-
+        
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery] UserParams userParams)
         {
@@ -46,7 +46,7 @@ namespace API.Controllers
 
             return Ok(users);
         }
-
+        
         [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DateInputComponent } from './_forms/date-input/date-input.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
+import { RolesModalsComponent } from './modals/roles-modals/roles-modals.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +48,11 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     PhotoEditorComponent,
     TextInputComponent,
     DateInputComponent,
-    MemberMessagesComponent               
+    MemberMessagesComponent,
+    AdminPanelComponent,
+    HasRoleDirective,
+    UserManagementComponent,
+    RolesModalsComponent               
   ],
   imports: [
     BrowserModule,
