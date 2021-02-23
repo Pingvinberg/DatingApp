21f0b065b875853df6d719f43134c4d64bb0dce2
Pingvinberg <Pingvinberg@gmail.com>
2021-02-23 11:31:40 +0100

    seed data

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -1,6 +1,4 @@
 using System.Collections.Generic;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using API.Entities;
@@ -11,27 +9,28 @@ namespace API.Data
 {
     public class Seed
     {
-        public static async Task SeedUsers(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
+        public static async Task SeedUsers(UserManager<AppUser> userManager, 
+            RoleManager<AppRole> roleManager)
         {
-            if(await userManager.Users.AnyAsync()) return;
+            if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
-            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);            
-            if(users == null) return;
+            var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+            if (users == null) return;
 
             var roles = new List<AppRole>
             {
-                new AppRole {Name = "Member"},
-                new AppRole {Name = "Admin"},
-                new AppRole {Name = "Moderator"}
+                new AppRole{Name = "Member"},
+                new AppRole{Name = "Admin"},
+                new AppRole{Name = "Moderator"},
             };
 
-            foreach(var role in roles)
+            foreach (var role in roles)
             {
                 await roleManager.CreateAsync(role);
             }
-
-            foreach(var user in users)
+            
+            foreach (var user in users)
             {
                 user.UserName = user.UserName.ToLower();
                 await userManager.CreateAsync(user, "Vinter21");
