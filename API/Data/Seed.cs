72654239b1e8bc41f427a18f970151b2166409d6
Pingvinberg <Pingvinberg@gmail.com>
2021-02-23 13:26:50 +0100

    seed

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -15,6 +15,7 @@ namespace API.Data
             if (await userManager.Users.AnyAsync()) return;
 
             var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+            
             var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
             if (users == null) return;
 
